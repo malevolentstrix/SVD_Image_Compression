473d4d438528eff05a0affd22d5ca64345e99182
Govdroid <85785236+Govdroid@users.noreply.github.com>
2021-06-13 10:27:30 +0530

    Update SVD_code.m

--- SVD_code.m
+++ SVD_code.m
@@ -1,5 +1,7 @@
-clear all, close all, clc
-A=imread('bridge.ppm');
+clear all;
+close all;
+clc
+A=imread('bridge.png');
 X=im2gray(A); % Convert RBG to gray, 256 bit to double.
 Y=im2double(X);
 nx = size(Y,1); ny = size(Y,2);
@@ -8,14 +10,29 @@ figure, subplot(2,3,1)
 imagesc(Y), axis off, colormap gray
 title('Original')
 plotind = 2;
-for r=[10 40 300 400 1637]; % Truncation value
- Xapprox = U(:,1:10)*S(1:10,1:10)*V(:,1:10)'; % Approx. image
+for r=[10 40 100 150 296]; % Truncation value
+ Xapprox = U(:,1:r)*S(1:r,1:r)*V(:,1:r)'; % Approx. image
  subplot(2,3,plotind), plotind = plotind + 1;
  imagesc(Xapprox), axis off
  title(['r=',num2str(r,'%d'),', ',num2str(100*r*(nx+ny)/(nx*ny),'%2.2f'),'% storage']);
+ %error(r)=immse(Xapprox,Y);
+ %psne(r)=psnr(Xapprox,Y);
 end
 set(gcf,'Position',[100 100 550 400])
 %psne=psnr(Y,Xapprox)
 %error=immse(Y,Xapprox)
+
+R=1:450;
+   for i=1:450
+       Xap=U(:,1:i)*S(1:i,1:i)*V(:,1:i)'; 
+       error(i)=immse(Xap,Y);
+       psne(i)=psnr(Xap,Y);
+   end
+   figure
+   plot(R,psne); hold on
+   figure
+   plot(R,error); hold on
+
+
 
 
